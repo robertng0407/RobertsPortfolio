53d819b11b719755faa9b7eb3ef87d8a06852e30
Robert Nguyen <robertng0407@gmail.com>
2017-09-10 14:26:03 -0700

    Integrate technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ puts "9 portfolio items created"
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technology items created"
